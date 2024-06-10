a95d628c6e0325da094c26aa7acd773f9f968377
infinity-boi <gouravsinghc2003@gmail.com>
2024-06-10 23:20:20 +0530

    3- User Controller (Backend)

---
+++ backend/routes/user.routes.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import { protectRoute } from '../middleware/protectRoute.js';
+import { getUserProfile, followUnfollowUser, getSuggestedUsers } from '../controllers/user.controller.js';
+
+const router = express.Router();
+
+router.get("/profile/:username", protectRoute, getUserProfile)
+router.get("/suggested", protectRoute, getSuggestedUsers)
+router.post("/follow/:id", protectRoute, followUnfollowUser)
+
+export default router;
